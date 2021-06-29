1a5a18b678cd8d30fe3ab8c815bbab4577f5e388
ymj <2513667783@qq.com>
2021-06-29 17:18:08 +0800

    增加Hystrix Dashboard：断路器执行监控

--- springcloud05-hystrix-dashboard/src/main/java/com/ymj/Springcloud05HystrixDashboardApplication.java
+++ springcloud05-hystrix-dashboard/src/main/java/com/ymj/Springcloud05HystrixDashboardApplication.java
@@ -4,20 +4,17 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
-/**
- * 在启动类上添加@EnableCircuitBreaker来开启Hystrix的断路器功能
- */
-@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Springcloud05HystrixServiceApplication {
+public class Springcloud05HystrixDashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springcloud05HystrixServiceApplication.class, args);
+        SpringApplication.run(Springcloud05HystrixDashboardApplication.class, args);
     }
 
 }
