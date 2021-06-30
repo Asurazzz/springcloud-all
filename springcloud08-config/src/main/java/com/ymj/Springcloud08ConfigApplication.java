5a0aa82eadac5518aecc49af736d05170b21bf27
ymj <2513667783@qq.com>
2021-06-30 09:45:21 +0800

    springcloud config配置

---
+++ springcloud08-config/src/main/java/com/ymj/Springcloud08ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ymj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud08ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud08ConfigApplication.class, args);
+    }
+
+}
