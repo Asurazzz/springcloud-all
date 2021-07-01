298c4c04b976a1e1388810adebbe116f245a19dd
ymj <2513667783@qq.com>
2021-07-01 11:38:16 +0800

    Spring Cloud Security：Oauth2使用入门

---
+++ springcloud12-oauth2-server/src/main/java/com/ymj/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.ymj.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname UserController
+ * @Description TODO
+ * @Date 2021/6/30 19:53
+ * @Created by yemingjie
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+}
